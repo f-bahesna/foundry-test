36812e8cebf5f16da393a168249ae689deeee76b
Lianum Frada Bahesna <fbahezna@gmail.com>
2025-01-13 12:35:06 +0700

    add test Hello

---
+++ src/Hello.sol
@@ -0,0 +1,6 @@
+
+pragma solidity ^0.8.17;
+
+contract Hello {
+	string public greet = "Hello";
+}
---
+++ test/Hello.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Hello.sol";
+
+contract HelloTest is Test {
+	Hello public hello;
+
+	function setUp() public {
+		hello = new Hello();
+	}
+
+	function testHello() public {
+		assertEq(
+			hello.greet(),
+			"Hello"
+		);
+	}
+}
