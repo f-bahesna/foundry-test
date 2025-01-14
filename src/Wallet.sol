206d5f554668c28d2e2ae4afd7235dfb942639a6
Lianum Frada Bahesna <fbahezna@gmail.com>
2025-01-14 12:31:24 +0700

    test: add authentication test

---
+++ src/Wallet.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.18;
+
+contract Wallet {
+    address payable public owner;
+
+    constructor() payable {
+        owner = payable(msg.sender);
+    }
+
+    receive() external payable {}
+
+    function withdraw(uint256 _amount) external {
+        require(msg.sender == owner, "caller is not an owner");
+        payable(msg.sender).transfer(_amount);
+    }
+
+    function setOwner(address _owner) external {
+        require(msg.sender == owner, "caller is not an owner");
+        owner = payable(_owner);
+    }
+}
