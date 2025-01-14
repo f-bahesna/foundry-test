1ec1f9d124d69004a58f85fbbcfb85c96e3821af
Lianum Frada Bahesna <fbahezna@gmail.com>
2025-01-14 09:14:22 +0700

    enhance: fix contract constructor and update gitignore

---
+++ src/Imports.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.17;
+
+import "solmate/tokens/ERC20.sol";
+
+contract Token is ERC20("name", "symbol", 18) {}
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TestOZ is Ownable {
+
+	constructor() Ownable(msg.sender){
+
+        }
+}
