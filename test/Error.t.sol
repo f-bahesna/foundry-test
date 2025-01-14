3ff8185d1b0f9f57bc5777e70880423061f15633
Lianum Frada Bahesna <fbahezna@gmail.com>
2025-01-14 14:59:06 +0700

    test: add error test

---
+++ test/Error.t.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+import {Error} from "../src/Error.sol";
+
+contract ErrorTest is Test {
+    Error public err;
+
+    function setUp() public {
+        err = new Error();
+    }
+
+    function testFail() public {
+        err.throwError();
+    }
+
+    function testRevert() public {
+        vm.expectRevert();
+        err.throwError();
+    }
+
+    function testRequireMessage() public {
+        vm.expectRevert(bytes("not authorized"));
+        err.throwError();
+    }
+
+    function testCustomError() public {
+        vm.expectRevert(Error.NotAuthorized.selector);
+        err.throwCustomError();
+    }
+
+    function testErrorLabel() public {
+        assertEq(uint256(1), uint256(1), "test 1 = 1");
+        assertEq(uint256(1), uint256(1), "test 1 = 1");
+        assertEq(uint256(1), uint256(1), "test 1 = 1");
+	assertEq(uint256(2), uint256(2), "test 2 = 2");
+    }
+}
